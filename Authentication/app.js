629cc22e16f0a150dd5c92db50c1fb9cf0483f9c
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2021-12-20 17:26:17 +0600

    Level 3 - Hashing using md5

--- Authentication/app.js
+++ Authentication/app.js
@@ -2,9 +2,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser= require("body-parser");
 const mongoose = require("mongoose");
+const md5 = require("md5");
 
-//for Lavel 2
-const encrypt = require("mongoose-encryption")
 
 const app = express();
 app.use(express.static("public"));
@@ -17,9 +16,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "ThisIsMySecretKey";
-//These line must be added before the Schema
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,11 +32,12 @@ app.get('/register', function(req, res){
   res.render("register");
 });
 
-//Lavel 2 Authentication: DataBase Encryption using Mongoose-Encryption (npm package)
+//Level 3: Hashing password
+
 app.post('/register', function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -53,7 +50,7 @@ app.post('/register', function(req, res){
 
 app.post('/login', function(req, res){
   const userName = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:userName}, function(err, foundUser){
     if(err){
@@ -73,38 +70,3 @@ app.post('/login', function(req, res){
 app.listen(3000, function() {
   console.log("Server has started on port 3000");
 });
-
-
-/*
-//Lavel 1 Authentication using email and password
-app.post('/register', function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  });
-});
-
-app.post('/login', function(req, res){
-  const userName = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({email:userName}, function(err, foundUser){
-    if(err){
-      console.log(err);
-    }else{
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
-      }
-    }
-  });
-});
-*/
