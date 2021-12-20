1e13910be9beec882795a927f330f8db537cab8f
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2021-12-20 22:57:41 +0600

    Uprade Level 2 With Environmental Variable (DotENV)

--- Authentication/app.js
+++ Authentication/app.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser= require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const encrypt = require("mongoose-encryption")
 
 
 const app = express();
@@ -15,6 +16,8 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+//These line must be added after the Schema
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -32,12 +35,11 @@ app.get('/register', function(req, res){
   res.render("register");
 });
 
-//Level 3: Hashing password
 
 app.post('/register', function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: md5(req.body.password)
+    password: req.body.password
   });
   newUser.save(function(err){
     if(err){
@@ -50,7 +52,7 @@ app.post('/register', function(req, res){
 
 app.post('/login', function(req, res){
   const userName = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email:userName}, function(err, foundUser){
     if(err){
