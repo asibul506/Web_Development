38d310c2383fc00afd571e49388c2b68bcf5679c
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2021-12-21 12:58:48 +0600

    Level 4: Hashing and salting using bcrypt

--- Authentication/app.js
+++ Authentication/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser= require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 
 const app = express();
@@ -11,17 +12,14 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true}));
 
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-//These line must be added after the Schema
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 
 const User = new mongoose.model("User", userSchema);
-
 
 app.get('/', function(req, res){
   res.render("home");
@@ -37,16 +35,18 @@ app.get('/register', function(req, res){
 
 
 app.post('/register', function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
   });
 });
 
@@ -59,13 +59,16 @@ app.post('/login', function(req, res){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result===true){
+              res.render("secrets");
+          }
+        });
         }
       }
-    }
   });
 });
+
 
 
 
