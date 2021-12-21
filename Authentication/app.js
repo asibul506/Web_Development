f6d43ec390c75a5fd5e9061cce040a930163a4d3
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2021-12-22 03:39:50 +0600

    Finishing up : let user to submit secrets

--- Authentication/app.js
+++ Authentication/app.js
@@ -1,4 +1,4 @@
-//Level 6: OAuth2.0 and implementing sign in using google
+//Finishing up : let user to submit secrets
 require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
@@ -31,7 +31,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 
 //settign up userSchema using passport-local-mongoose as a plugin
@@ -94,8 +95,19 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+  User.find({"secret": {$ne:null}}, function(err, foundUsers){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUsers){
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+app.get("/submit", function(req, res){
   if(req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }else{
     res.redirect("/login");
   }
@@ -105,6 +117,23 @@ app.get("/logout", function(req, res){
   req.logout();
   res.redirect("/");
 })
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret  = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
 
 
 app.post('/register', function(req, res){
